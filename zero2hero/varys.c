ddf00447d5edcc1d0dfdd9deb8786a02de70d737
code-saahe <165862365+code-saahe@users.noreply.github.com>
2025-09-28 15:59:11 -0400

    vary

--- zero2hero/varys.c
+++ zero2hero/varys.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+// global scope
 
 int main(int argc, char **argv)
 {
     int personID = 1;
+    int personID2 = personID + 1;
     printf("This is the PersonId badge num: %d\n", personID);
+    printf("This is the PersonId badge num: %d\n", personID2);
+    printf("This is the PersonId badge num: %d\n", personID2 += 2);
     return 0;
 }
