cf1ff345cf99392b3662704a09d158746324a3f8
code-saahe <165862365+code-saahe@users.noreply.github.com>
2025-09-28 22:28:58 -0400

    funcs

---
+++ zero2hero/func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbit.h>
+
+void print_binary_func()
+{
+    size_t let = 9;
+    printf("%ld\n", let);
+    return;
+}
+int main()
+{
+    print_binary_func();
+    return 0;
+}
